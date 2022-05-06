c4a52f3417ee765ee98e95c6c14394077406a38f
coderjibon <contact.engjibon@gmail.com>
2022-05-06 20:34:12 +0600

    modify done

--- assets/js/script.js
+++ assets/js/script.js
@@ -288,6 +288,41 @@
     });
   }
 
+
+  // banner-carousel 2
+  
+    $(".banner-carousel-cd").owlCarousel({
+      loop: true,
+      margin: 0,
+      nav: true,
+      animateOut: "fadeOut",
+      animateIn: "fadeIn",
+      active: true,
+      smartSpeed: 1000,
+      autoplay: 6000,
+      navText: [
+        '<span class="flaticon-arrowhead-thin-outline-to-the-left"></span>',
+        '<span class="flaticon-right-arrow-2"></span>',
+      ],
+      responsive: {
+        0: {
+          items: 1,
+        },
+        600: {
+          items: 1,
+        },
+        800: {
+          items: 1,
+        },
+        1024: {
+          items: 1,
+        },
+      },
+    });
+
+
+
+
   // location-carousel
   if ($(".location-carousel").length) {
     $(".location-carousel").owlCarousel({
